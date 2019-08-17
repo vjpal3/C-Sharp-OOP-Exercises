514effb1e8bc3a0dcfe9c6f006e2ccbb09c6d4ca
vjpal3 <vrishalipal@yahoo.com>
2019-08-17 08:12:09 -0400

    Stackoverflow post with upvote and downvote functionality

---
+++ StackOverflowPost.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesSection2
+{
+    class StackOverflowPost
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+
+        private int vote;
+
+        public StackOverflowPost(string title, string description) 
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Created = DateTime.Now;
+        }
+
+        public void Upvote()
+        {
+            vote++;
+        }
+
+        public void Downvote()
+        {
+            vote--;
+        }
+
+        public int GetVote()
+        {
+            return vote;
+        }
+    }
+}
