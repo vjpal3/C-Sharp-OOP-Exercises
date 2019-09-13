972698f25b5a099b5c6afdac320203b7359770c7
vjpal3 <vrishalipal@yahoo.com>
2019-09-13 12:24:14 -0400

    Use Indexer as property in a class

---
+++ HttpCookie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Indexers
+{
+
+    // HttpCookie class is a wrapper around dictionary, with some added functionality. 
+    // It uses dictionary for data storage.
+    class HttpCookie
+    {
+        private readonly Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        public DateTime Expiry { get; set; }
+
+        // define indexer
+        public string this[string key]
+        {
+            get { return dictionary[key]; }
+            set { dictionary[key] = value;  }
+        }
+    }
+}
