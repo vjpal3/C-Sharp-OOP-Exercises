99ce8a70cac181a5fa57c67151936b25d5190ffb
vjpal3 <vrishalipal@yahoo.com>
2019-09-13 09:00:16 -0400

    Restrict properties' set-accessor methods

--- Person.cs
+++ Person.cs
@@ -8,14 +8,23 @@ namespace Exercises.Property
 {
     class Person
     {
-        public DateTime Birthdate { get; set; }
+        public DateTime Birthdate { get; private set; }
         public int Age
         {
+            // Not an auto implemented property.
+            // Age is calulated and returned.
+            // no set accessor method defined so the person object is always in a valid state.
             get
             {
                 var timespan = DateTime.Today - Birthdate;
                 return timespan.Days / 365;
             }
+        }
+
+        public Person(DateTime birthdate)
+        {
+            // Set Birthdate only once here so the person object is always in a valid state.
+            Birthdate = birthdate;
         }
     }
 }
