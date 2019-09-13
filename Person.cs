28fe8f43e8a6087408fdb2904d11172bb2cf3132
vjpal3 <vrishalipal@yahoo.com>
2019-09-13 08:32:08 -0400

    Use Properties to encapsulate class fields

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Property
+{
+    class Person
+    {
+        public DateTime Birthdate { get; set; }
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                return timespan.Days / 365;
+            }
+        }
+    }
+}
