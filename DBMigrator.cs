a33b1dac5ebb48f09f8a8c24512d4eb2d8b948fd
vjpal3 <vrishalipal@yahoo.com>
2019-09-13 22:13:08 -0400

    Reuse classes using composition

---
+++ DBMigrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercises.Composition;
+
+namespace Exercises.Composition
+{
+    class DBMigrator
+    {
+        private readonly Logger logger;
+
+        public DBMigrator(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Migrate()
+        {
+            logger.Log("Migrating database...");
+        }
+    }
+}
---
+++ Installer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Composition
+{
+    class Installer
+    {
+        private readonly Logger logger;
+
+        public Installer(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Install()
+        {
+            logger.Log("Installation started....");
+        }
+    }
+}
