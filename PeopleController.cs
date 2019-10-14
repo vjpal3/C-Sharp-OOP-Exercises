87dd7a75d451f5a423cbba3c06bfd5060fb99376
Vrishali Pal <vrishalipal@yahoo.com>
2019-10-13 22:09:09 -0400

    ASP.Net MVC Controller

---
+++ PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Rose", LastName = "Anderson", Age = 20 });
+            people.Add(new PersonModel { FirstName = "Ashka", LastName = "Thomas", Age = 33 });
+            people.Add(new PersonModel { FirstName = "Dawn", LastName = "Robins", Age = 45 });
+
+            return View(people);
+        }
+    }
+}
